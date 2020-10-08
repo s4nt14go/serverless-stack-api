f6849f6fe62f9f25ed9a965baf1c8816e7128c1c
s4nt14go <santiagopergamo@gmail.com>
2020-10-08 09:14:53 -0300

    Revert "Error Logging"
    This reverts commit b49d01e4

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,20 +1,13 @@
-import * as debug from "./debug-lib";
-
 export default function handler(lambda) {
   return async function (event, context) {
     let body, statusCode;
-
-    // Start debugger
-    debug.init(event, context);
 
     try {
       // Run the Lambda
       body = await lambda(event, context);
       statusCode = 200;
     } catch (e) {
-      // Print debug messages
-      debug.flush(e);
-
+      console.log(e);
       body = { error: e.message };
       statusCode = 500;
     }
